3d336f84df124366f58771e4bd342c313fd5e807
AndrewThePug1 <andrewmondejarp@gmail.com>
2025-08-05 20:01:46 -0400

    Add UpdatedDropdownDown UpdatedDropdown WindowsActivities

---
+++ src/Sel/WindowsActivities.java
@@ -0,0 +1,21 @@
+package Sel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowsActivities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:/Users/andre/Desktop/chromedriver-win64/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+		driver.navigate().to("https://rahulshettyacademy.com/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		
+		
+	}
+
+}
